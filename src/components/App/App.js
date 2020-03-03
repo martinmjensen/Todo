e6366c3464737372004a41c9680207c0dbf2df7f
martinjensen <martinmyrvold@me.com>
2020-03-03 16:10:54 +0100

    added header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Provider } from 'react-redux'
 
 import store from '../../store/store'
+import Header from '../Header/Header'
 import TodoContainer from '../Todo/TodoContainer'
 
 const App = () => {
     return (
         <Provider store={store}>
-            <h1>Todo, or not todo.</h1>
+            <Header />
             <TodoContainer />
         </Provider>
     )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className='title'>Todo.</h1>
+            <h2 className='sub-title'>Todo, or not todo, that is the question.</h2>
+        </header>
+    )
+}
+
+export default Header
