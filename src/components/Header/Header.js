8e1df87deacf74db577701256f35f1875f4cd802
martinjensen <martinmyrvold@me.com>
2020-03-05 16:42:38 +0100

    fixed spelling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.scss'
 const Header = () => (
     <header>
         <h1 className='title'>Todo.</h1>
-        <h2 className='sub-title'>Todo, or not todo, that is the question?</h2>
+        <h2 className='sub-title'>Todo, or not todo, that is the question.</h2>
     </header>
 )
 
