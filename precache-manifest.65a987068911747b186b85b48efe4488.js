0cb90c23c8afd9d5fe9b0fff2f215dc120ec2bec
martinjensen <martinmyrvold@me.com>
2020-03-05 15:43:06 +0100

    Updates

---
+++ precache-manifest.65a987068911747b186b85b48efe4488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fa136ecd2ced0692d2623f5909bc2e",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "9202e8dd158620ea0d98",
+    "url": "/Todo/static/css/main.9dc2fa46.chunk.css"
+  },
+  {
+    "revision": "b3557b4632749c7ef28b",
+    "url": "/Todo/static/js/2.4da96c34.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Todo/static/js/2.4da96c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9202e8dd158620ea0d98",
+    "url": "/Todo/static/js/main.331cf9c8.chunk.js"
+  },
+  {
+    "revision": "50c77502d4a4a920777f",
+    "url": "/Todo/static/js/runtime-main.4fa7160f.js"
+  }
+]);
