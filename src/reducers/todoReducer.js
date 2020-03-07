60af2a1a7364c598b945f2258ab6ebd163dce494
martinjensen <martinmyrvold@me.com>
2020-03-07 13:38:48 +0100

    fixed immutability in reducer

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,4 +1,5 @@
 import * as types from '../actions/actionTypes'
+import update from 'immutability-helper'
 
 export const initialState = {
     isLoading: false,
@@ -7,52 +8,39 @@ export const initialState = {
 }
 
 const todoReducer = (state = initialState, action) => {
-    let newState = { ...state }
     let { type, payload } = action
 
     switch (type) {
         // ---- When initial request action is triggered 
         case types.GET_TODOS:
-            newState.isLoading = true
-            return newState
         case types.DELETE_TODO:
         case types.TOGGLE_TODO:
         case types.ADD_TODO:
-            newState.isLoading = true
-            return newState
+            return { ...state, isLoading: true, }
 
         // ---- When SUCCESS actions are triggered 
         case types.GET_TODOS_SUCCEEDED:
-            newState.todos = payload.todos
-            newState.isLoading = false
-            return newState
+            const getState = update(state, { todos: { $set: payload.todos } })
+            return { ...getState, isLoading: false, }
         case types.DELETE_SUCCEEDED:
-            const todoIndex1 = newState.todos.findIndex(t => t.id === payload.id)
-            newState.todos.splice(todoIndex1, 1)
-            newState.isLoading = false
-            return newState
+            const todoIndex1 = state.todos.findIndex(t => t.id === payload.id)
+            const deleteState = update(state, { todos: { $splice: [[todoIndex1, 1]] } })
+            return { ...deleteState, isLoading: false, }
         case types.TOGGLE_SUCCEEDED:
-            const todoIndex2 = newState.todos.findIndex(t => t.id === payload.todo.id)
-            newState.todos.splice(todoIndex2, 1, payload.todo)
-            newState.isLoading = false
-            return newState
+            const todoIndex2 = state.todos.findIndex(t => t.id === payload.todo.id)
+            const toggleState = update(state, { todos: { $splice: [[todoIndex2, 1, payload.todo]] } })
+            return { ...toggleState, isLoading: false, }
         case types.ADD_TODO_SUCCEEDED:
-            newState.todos.push(payload.todo)
-            newState.isLoading = false
-            return newState
+            const addState = update(state, { todos: { $push: [payload.todo] } })
+            return { ...addState, isLoading: false, }
 
         // ---- When FAIL actions are triggered    
         case types.GET_TODOS_FAILED:
-            newState.error = payload.error
-            newState.isLoading = false
-            return newState
         case types.DELETE_FAILED:
         case types.TOGGLE_FAILED:
         case types.ADD_TODO_FAILED:
             console.error(`Server error: ${payload.error}`)
-            newState.error = payload.error
-            newState.isloading = false
-            return newState
+            return { ...state, error: payload.error, isLoading: false, }
 
         default:
             return state
